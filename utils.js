bad8effcf3d636a2a6d32cfcef06e6526a35156a
Zsolt <zsolt@intense>
2015-11-24 23:27:37 +0100

    utilities for stratified sampling

--- utils.js
+++ utils.js
@@ -25,3 +25,52 @@ pad = function(num, size) {
   var s = "000000000" + num;
   return s.substr(s.length - size);
 };
+
+// Sample n random values from a collection using the modern version of the
+// [Fisher-Yates shuffle](http://en.wikipedia.org/wiki/Fisher–Yates_shuffle).
+randomSample = function(obj, n) {
+  var sample = obj.slice(0);
+  var length = sample.length;
+  n = Math.max(Math.min(n, length), 0);
+  var last = length - 1;
+  for (var index = 0; index < n; index++) {
+    var rand = randInt(index, last);
+    var temp = sample[index];
+    sample[index] = sample[rand];
+    sample[rand] = temp;
+  }
+  return sample.slice(0, n);
+};
+
+
+stratifiedSample = function(x, n) {
+  var ax = new Array(x);
+  for (var j = 0; j < x; ++j) {
+    ax[j] = j;
+  }
+  var sx = sample(ax, x % n);
+  sx.sort().reverse();
+  for (var j in sx) {
+    ax.splice(sx[j], 1);
+  }
+  var d = Math.floor(ax.length / n);
+  var s = new Array(n);
+  for (var j = 0; j < n; ++j) {
+    var i = d * j + Math.floor(Math.random() * d);
+    s[j] = ax[i];
+  }
+  return s;
+};
+
+multiStratifiedSample = function(x, y, n) {
+	var p = Math.floor(Math.sqrt(n)) + 1;
+  var pairs = [];
+  for (var i = 0; i < p; ++i) {
+		var s = stratifiedSample(x, p);
+	  for (var j = 0; j < p; ++j) {
+      var t = stratifiedSample(y, p);
+      pairs.push([s[j], t[i]]);
+    }
+  }
+  return sample(pairs, n);
+};
