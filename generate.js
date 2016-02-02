6719b28c467e4b37dc9a5e8365e4917fd5a63076
Zsolt Terek <zsolt@zsolt-macbookair.roam.corp.google.com>
2016-02-02 06:48:31 +0100

    generation updated

--- generate.js
+++ generate.js
@@ -1,9 +1,20 @@
 generate = function() {
-  var link = 'index.html#' + $('#form').serialize();
+  var domain = $('#form-maths').css('display') == 'none' ? 'music' : 'maths';
+  var link = domain + '.html#' +
+    $('#form').serialize() + '&' +
+    $('#form-' + domain).serialize();
   $('#link').attr('href', link);
   $('#link').text(link);
 }
 
+showPane = function(name) {
+  $('#form-maths').toggle(name == 'maths');
+  $('#form-music').toggle(name == 'music');
+};
+
 $(document).ready(function() {
   $('#generate').bind('click', generate);
+  showPane('maths');
+  $('#radioMaths').bind('click', function() { showPane('maths'); });
+  $('#radioMusic').bind('click', function() { showPane('music'); });
 });
